bc47055721a92ae27b53cc454f90ab3407046a83
Jeovana Santos da Costa <jeovanasantosdacosta25@gmail.com>
2024-08-24 14:52:36 -0400

    botao reiniciar

--- app.js
+++ app.js
@@ -35,3 +35,10 @@ function alterarStatusBotao(){
 
   }
 }
+function reiniciar(){
+  document.getElementById("quantidade").value = '';
+  document.getElementById("de").value = '';
+  document.getElementById("ate").value = '';
+  document.getElementById("resultado").innerHTML = '<label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>';
+  alterarStatusBotao();
+}
